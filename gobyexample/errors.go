60b25bff8d7d0b39fef24034d2f422cdae06e840
Ashraful Islam Mahim <ashrafulmahim@gmail.com>
2025-03-19 02:33:27 +0600

    errors in go

---
+++ gobyexample/errors.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+)
+
+func f(arg int) (int, error) {
+	if arg == 42 {
+		return -1, errors.New("can't work with 42")
+	}
+
+	return arg + 3, nil
+}
+
+var ErrOutOfTea = fmt.Errorf("No more tea available")
+var ErrPower = fmt.Errorf("Can't boil water")
+
+func makeTea(arg int) error {
+	fmt.Println("arg:", arg)
+	if arg == 2 {
+		return ErrOutOfTea
+	} else if arg == 4 {
+		return fmt.Errorf("making tea: %w", ErrPower)
+	}
+
+	return nil
+}
+
+func errors_main() {
+	for index, i := range []int{7, 42} {
+		fmt.Println(index, i)
+		if r, e := f(i); e != nil {
+			fmt.Println("f failed:", e)
+		} else {
+			fmt.Println("f worked:", r)
+		}
+	}
+
+	for i := range 5 {
+		if err := makeTea(i); err != nil {
+			
+			if errors.Is(err, ErrOutOfTea) {
+				fmt.Println("We should buy new tea!")
+			} else if errors.Is(err, ErrPower) {
+				fmt.Println("Now it is dark")
+			} else {
+				fmt.Println("Unknown error: %s\n", err)
+			}
+
+			continue
+		}
+
+		fmt.Println("Tea is ready!")
+	}
+}
